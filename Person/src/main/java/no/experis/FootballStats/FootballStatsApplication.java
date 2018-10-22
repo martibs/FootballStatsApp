54c561b614e16fe517831586d50f1ce4ae840042
martibs <masundf@gmail.com>
2018-10-22 14:30:40 +0200

    add(feat: CORS Configuration)

--- Person/src/main/java/no/experis/FootballStats/FootballStatsApplication.java
+++ Person/src/main/java/no/experis/FootballStats/FootballStatsApplication.java
@@ -2,6 +2,8 @@ package no.experis.FootballStats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 
@@ -11,4 +13,15 @@ public class FootballStatsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FootballStatsApplication.class, args);
 	}
+
+	@Bean
+	public FilterRegistrationBean corsFilterRegistration(){
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(new CORSFilter());
+		registrationBean.setName("CORS Filter");
+		registrationBean.addUrlPatterns("/*");
+		registrationBean.setOrder(1);
+		return  registrationBean;
+	}
+
 }
+
