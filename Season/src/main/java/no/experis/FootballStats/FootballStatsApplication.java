50493716700782aa08b4c2f6b365d439c49ca639
martibs <masundf@gmail.com>
2018-10-22 15:04:00 +0200

    add(CORSFilter on all microservices)

--- Season/src/main/java/no/experis/FootballStats/FootballStatsApplication.java
+++ Season/src/main/java/no/experis/FootballStats/FootballStatsApplication.java
@@ -2,6 +2,8 @@ package no.experis.FootballStats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 
@@ -10,5 +12,14 @@ public class FootballStatsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FootballStatsApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean corsFilterRegistration(){
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(new CORSFilter());
+		registrationBean.setName("CORS Filter");
+		registrationBean.addUrlPatterns("/*");
+		registrationBean.setOrder(1);
+		return  registrationBean;
 	}
 }
